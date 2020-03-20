4e9ac1784d6f950e12042245991d204dce417a17
dkulazhenko <dkulazhenko@cloudlinux.com>
2020-03-20 17:27:24 +0300

    Minor improvements

--- src/main/java/ru/itmo/java/HashTable.java
+++ src/main/java/ru/itmo/java/HashTable.java
@@ -2,21 +2,12 @@ package ru.itmo.java;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class HashTable {
-    private static class Entry {
-        private Object key, value;
-
-        public Entry(Object key, Object value) {
-            this.key = key;
-            this.value = value;
-        }
-    }
-
-    private final int MAX_INIT_SIZE = 1_000_007;
-    private final int INIT_SIZE = 4096;
-    private final int RESIZE_FACTOR = 2;
-    private final int HASH_STEP = 911;
-    private final float INIT_LOAD_FACTOR = 0.5f;
-    private final float THRESHOLD = 0.75f;
+    private final static int INIT_SIZE = 4096;
+    private final static int RESIZE_FACTOR = 2;
+    private final static int HASH_STEP = 911;
+    private final static float INIT_LOAD_FACTOR = 0.5f;
+    private final static float THRESHOLD = 0.75f;
+    private static final Entry DELETED = new Entry(null, null);
 
     private int size = 0;
     private int realSize = 0;  // Including deleted entries
@@ -24,14 +15,11 @@ public class HashTable {
     private float loadFactor;
 
     public HashTable() {
-        this.loadFactor = this.INIT_LOAD_FACTOR;
-        this.table = new Entry[this.INIT_SIZE];
+        this(INIT_SIZE, INIT_LOAD_FACTOR);
     }
 
     public HashTable(int initSize) {
-        validateInitParams(initSize, null);
-        this.loadFactor = this.INIT_LOAD_FACTOR;
-        this.table = new Entry[initSize];
+        this(initSize, INIT_LOAD_FACTOR);
     }
 
     public HashTable(int initSize, float loadFactor) {
@@ -40,42 +28,14 @@ public class HashTable {
         this.table = new Entry[initSize];
     }
 
-    private void validateInitParams(Integer initSize, Float loadFactor) {
-        if (initSize != null && (initSize <= 0 || initSize > this.MAX_INIT_SIZE)) {
-            throw new IllegalArgumentException("Initial size must be in (0; " + this.MAX_INIT_SIZE + "]");
+    public Object put(Object key, Object value) {
+        if (this.table.length * this.loadFactor < this.size) {
+            this.resize(RESIZE_FACTOR);
         }
-        if (loadFactor != null && (loadFactor <= 0 || loadFactor > 1)) {
-            throw new IllegalArgumentException("Load factor must be in (0; 1]");
+        if (THRESHOLD > this.loadFactor && this.table.length * THRESHOLD < this.realSize) {
+            this.resize(1);
         }
-    }
 
-    private int getHash(Object key) {
-        int hash = key.hashCode() % this.table.length;
-        if (hash < 0) {
-            hash += this.table.length;
-        }
-        return hash;
-    }
-
-    private int nextHash(int hash) {
-        return (hash + this.HASH_STEP) % this.table.length;
-    }
-
-    private void resize(int factor) {
-        Entry[] oldTable = this.table;
-        this.table = new Entry[oldTable.length * factor];
-        this.size = 0;
-        this.realSize = 0;
-
-        for (Entry entry : oldTable) {
-            if (entry != null  && entry.key != null && entry.value != null) {
-                this.put(entry.key, entry.value);
-            }
-        }
-    }
-
-
-    Object put(Object key, Object value) {
         int hash = this.getHash(key);
 
         while (this.table[hash] != null) {
@@ -91,17 +51,10 @@ public class HashTable {
         ++this.realSize;
         ++this.size;
 
-        if (this.table.length * this.loadFactor < this.size) {
-            this.resize(this.RESIZE_FACTOR);
-        }
-        if (this.THRESHOLD > this.loadFactor && this.table.length * this.THRESHOLD < this.realSize) {
-            this.resize(1);
-        }
-
         return null;
     }
 
-    Object get(Object key) {
+    public Object get(Object key) {
         int hash = this.getHash(key);
         while (this.table[hash] != null) {
             if (key.equals(this.table[hash].key)) {
@@ -112,13 +65,12 @@ public class HashTable {
         return null;
     }
 
-    Object remove(Object key) {
+    public Object remove(Object key) {
         int hash = this.getHash(key);
         while (this.table[hash] != null) {
             if (key.equals(this.table[hash].key)) {
                 Object value = this.table[hash].value;
-                this.table[hash].key = null;
-                this.table[hash].value = null;
+                this.table[hash] = DELETED;
                 --size;
                 return value;
             }
@@ -127,7 +79,51 @@ public class HashTable {
         return null;
     }
 
-    int size() {
+    public int size() {
         return this.size;
+    }
+
+    private void validateInitParams(Integer initSize, Float loadFactor) {
+        if (initSize != null && initSize <= 0) {
+            throw new IllegalArgumentException("Initial size must be positive");
+        }
+        if (loadFactor != null && (loadFactor <= 0 || loadFactor > 1)) {
+            throw new IllegalArgumentException("Load factor must be in (0; 1]");
+        }
+    }
+
+    private int getHash(Object key) {
+        int hash = key.hashCode() % this.table.length;
+        if (hash < 0) {
+            hash += this.table.length;
+        }
+        return hash;
+    }
+
+    private int nextHash(int hash) {
+        return (hash + HASH_STEP) % this.table.length;
+    }
+
+    private void resize(int factor) {
+        Entry[] oldTable = this.table;
+        this.table = new Entry[oldTable.length * factor];
+        this.size = 0;
+        this.realSize = 0;
+
+        for (Entry entry : oldTable) {
+            if (entry != null  && entry.key != null && entry.value != null) {
+                this.put(entry.key, entry.value);
+            }
+        }
+    }
+
+    private static class Entry {
+        private Object key;
+        private Object value;
+
+        public Entry(Object key, Object value) {
+            this.key = key;
+            this.value = value;
+        }
     }
 }
